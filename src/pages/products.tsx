f38e6eec171fc2424ba5218ae0386e90e145dd91
Eduardo Brito <eduardo.guardian@gmail.com>
2021-05-03 03:03:19 -0300

    Update Products page to use getStaticProps and list products at server

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,8 +1,10 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import StyledLink from 'src/components/StyledLink'
+import api from 'src/services/api'
 
 
-export default function Home() {
+export default function Home({ productsData }) {
   return (
     <>
       <Head>
@@ -12,6 +14,33 @@ export default function Home() {
       <StyledLink href="/" forwardedAs="/">
         Home
       </StyledLink>
+      <ul>
+        {productsData.map(prod => (
+          <li>{prod.name}</li>
+        ))}
+      </ul>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await api
+    .get('catalog_system/pub/products/search?fq=C:2000094/4005284')
+    .then(res => res.data)
+  // console.log(products)
+  const productsData = products.map(prod => {
+    const params = {
+      id: prod.productId,
+      name: prod.productName,
+      slug: prod.linkText,
+      link: prod.link
+    }
+    return params
+  })
+  return {
+    props: {
+      productsData
+    },
+    // revalidate: 60 * 60 * 24,
+  }
 }
