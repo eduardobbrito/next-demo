2ff33c333640e57027f17b7ccc351703d24e7a5a
Eduardo Brito <eduardo.guardian@gmail.com>
2021-05-03 00:07:06 -0300

    Add styled-components, babelrc, global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from 'next/app'
+import GlobalStyle from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 // pages folder can only be at root, or inside /src
+import styled from 'styled-components'
+
+const Title = styled.h1` // var must begin with uppercase
+  color: red; // install vscode-styled-components by Julien Poissonnier
+  span {
+    color: green;
+    display: block;
+  }
+`
 
 export default function Home() {
   return (
-    <h1>Hello World with TS!</h1>
+    <Title>Hello World with TS!<span>Com styled-components</span></Title>
   )
 }
