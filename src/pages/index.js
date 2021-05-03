d08e95182b9a3b237886254e824bd756de81bc64
Eduardo Brito <eduardo.guardian@gmail.com>
2021-05-02 23:21:30 -0300

    First commit, clean next-app

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+// pages folder can only be at root, or inside /src
+
+export default function Home() {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
---
+++ src/pages/products.js
@@ -0,0 +1,8 @@
+// File system routing, each file inside /pages will be a route
+// except for _app and _document
+
+export default function Products() {
+  return (
+    <h1>Products</h1>
+  )
+}
